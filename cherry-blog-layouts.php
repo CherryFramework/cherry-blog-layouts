9b47fb7acad962ec0c842c25b83eb0bcbe71fb77
cheh <chehovskiy.dima@gmail.com>
2015-07-21 17:28:57 +0300

    Version bump to 1.0.1-beta

--- cherry-blog-layouts.php
+++ cherry-blog-layouts.php
@@ -4,7 +4,7 @@
  * Plugin Name: Cherry Blog Layouts
  * Plugin URI:  http://www.cherryframework.com/
  * Description: Additional blog layouts.
- * Version:     1.0.0
+ * Version:     1.0.1 beta
  * Author:      Cherry Team
  * Author URI:  http://www.cherryframework.com/
  * Text Domain: cherry-blog-layouts
@@ -94,7 +94,7 @@ if ( ! class_exists( 'Cherry_Blog_Layouts' ) ) {
 			 *
 			 * @since 1.0.0
 			 */
-			define( 'CHERRY_BLOG_VERSION', '1.0.0');
+			define( 'CHERRY_BLOG_VERSION', '1.0.1');
 
 			/**
 			 * Set the slug of the plugin.
