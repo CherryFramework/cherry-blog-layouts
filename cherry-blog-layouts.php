bbf7d17e76b4964e18c92a25b00a74f137b0a86a
makhonkodenis <behaart@corp.devoffice.com>
2015-08-10 12:56:19 +0300

    Fix base update class

--- cherry-blog-layouts.php
+++ cherry-blog-layouts.php
@@ -4,12 +4,12 @@
  * Plugin Name: Cherry Blog Layouts
  * Plugin URI:  http://www.cherryframework.com/
  * Description: Additional blog layouts.
- * Version:     1.0.1 beta
+ * Version:     1.0.1
  * Author:      Cherry Team
  * Author URI:  http://www.cherryframework.com/
  * Text Domain: cherry-blog-layouts
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:     GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  * Domain Path: /languages
  */
 
