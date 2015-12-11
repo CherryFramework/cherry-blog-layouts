c0c75ea8562b4bbb8975161210dbb7eb4b622dd8
shin <shin@devoffice.com>
2015-12-11 14:23:32 +0200

    fix: grid-layout ie

--- templates/layout-grid.php
+++ templates/layout-grid.php
@@ -20,15 +20,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	$parsed_options = Cherry_Blog_Layouts_Data::get_parsed_options();
 	echo Cherry_Blog_Layouts_Data::filter_render( $parsed_options['filter_type'] ); ?>
 
-	<?php
-		$post_counter = 0;
-		$columns = Cherry_Blog_Layouts::get_option( 'blog-layout-columns', 3 );
+		<?php
+			$post_counter = 0;
+			$columns = Cherry_Blog_Layouts::get_option( 'blog-layout-columns', 3 );
 
-		while ( have_posts() ) : the_post();
-			$template_file = Cherry_Blog_Template_Loader::get_template( 'layout-grid', 'content' );
-			include $template_file;
-		endwhile;
-	?>
+			while ( have_posts() ) : the_post();
+				$template_file = Cherry_Blog_Template_Loader::get_template( 'layout-grid', 'content' );
+				include $template_file;
+			endwhile;
+		?>
+		</div>
 </div>
 
 <?php
