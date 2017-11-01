14d81753ace1b84c902e54a19f8be547bca9b37e
mjhead <andremjhead@gmail.com>
2017-11-01 11:39:28 +0200

    FIX: translation

--- cherry-blog-layouts.php
+++ cherry-blog-layouts.php
@@ -3,10 +3,10 @@
  * Plugin Name: Cherry Blog Layouts
  * Plugin URI:  http://www.cherryframework.com/
  * Description: Additional blog layouts.
- * Version:     1.0.6
+ * Version:     1.0.7
  * Author:      Cherry Team
  * Author URI:  http://www.cherryframework.com/
- * Text Domain: cherry-blog-layouts
+ * Text Domain: cherry-blog
  * License:     GPL-3.0+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  * Domain Path: /languages
@@ -124,7 +124,7 @@ if ( ! class_exists( 'Cherry_Blog_Layouts' ) ) {
 			 *
 			 * @since 1.0.0
 			 */
-			define( 'CHERRY_BLOG_VERSION', '1.0.6' );
+			define( 'CHERRY_BLOG_VERSION', '1.0.7' );
 
 			/**
 			 * Set the slug of the plugin.
